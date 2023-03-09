4b034c0422b57d331f1aaaff5e4b7955603feb18
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-03-09 13:21:49 -0600

    Created Sign up form with inputs and styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Signup />
       <Login />
     </div>
   );
