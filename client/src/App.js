465031d4d74bfdc2a45d86dfea4f51477a0e6acd
Harrison Boatman <hrry.boatman2@gmail.com>
2023-03-09 13:39:52 -0600

    updated file structure

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Signup />
+      <Login />
+      
+    </div>
+  );
+}
+
+export default App;
