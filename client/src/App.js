aa8dd2e7c6196129778a418d16ac3cb89c20a171
connorbodin <connor.e.bodin@gmail.com>
2023-03-09 21:27:22 -0600

    added Home styling with pics :)

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './pages/Navbar'
 import Login from '../src/pages/Login';
 import Signup from '../src/pages/Signup';
+import Home from '../src/pages/Home';
 // import Signup from './pages/Signup.js';
 
 
@@ -9,8 +10,9 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Signup />
-      <Login />
+      {/* {/* <Signup /> */}
+      <Home />
+      {/* <Login /> */ }
       
     </div>
   );
