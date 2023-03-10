5dd259b020345be42cea23e9d78a3386a9964b1d
Medranomiler1 <medranomiler@gmail.com>
2023-03-09 20:52:05 -0600

    checkout page rough draft

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://harrisonboatman:Ranss5ul@cluster0.9z8sb8z.mongodb.net/jate', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://medranomiler:root1234@cluster0.kuljfho.mongodb.net/jate', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
